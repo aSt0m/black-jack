43c15a4794ede62e2a2560a7b076ca109adb1180
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-01-15 00:02:18 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -45,6 +45,34 @@ const pedirCarta = ()=>{
     let carta = deck.pop()
     console.log( deck )
     
+    console.log( carta )
     return carta
     }
-pedirCarta()
+
+
+function valorCarta( carta ){
+
+    let valor = carta.substring( 0, carta.length -1 );
+    console.log( valor );
+    let puntos = 0;
+    if( isNaN( valor ) ){
+
+        
+        if( valor === 'A' ){
+            valor = 11;
+            
+        }else{
+            valor = 10;
+        }
+        console.log( `El valor es de una letra, vale ${ valor }`);
+    
+    }else{
+        
+        console.log( "Es numero")
+        puntos = valor * 1 ;
+        console.log( puntos );
+    }
+
+
+}
+ valorCarta( "ds" );
