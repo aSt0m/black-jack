f54ad16469805ac0690b24c3f3507ead67221fa5
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-01-16 00:08:38 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,70 +9,121 @@
 //Q: Queen
 //K: King
 let deck = [];
-let tipos = ['H','D','S','C'];
-let especiales = ['A','J','Q','K'];
+let tipos = ['H', 'D', 'S', 'C'];
+let especiales = ['A', 'J', 'Q', 'K'];
 
-function crearDeck(){
+let puntosJugador = 0;
 
-    
-    for( let i = 2; i <=10; i++){
-        tipos.forEach((tipo)=>{
-        
-        deck.push( tipo + i )
+
+
+let btnPedirCarta = document.querySelector('.btn-pedir');
+let btnNuevo = document.querySelector('.btn-nuevo');
+let btnDetener = document.querySelector('.btn-detener');
+
+const divBotones = document.querySelector('#divBotones');
+const player = document.querySelector('.player');
+
+const divJugador = document.querySelector('.jugador');
+const divImg = document.querySelector('img')
+
+const siteHeader = document.querySelector('.site-header');
+
+function crearDeck() {
+
+
+    for (let i = 2; i <= 10; i++) {
+        tipos.forEach((tipo) => {
+
+            deck.push(i + tipo)
+        })
+
+    }
+    tipos.forEach((tipo) => {
+        especiales.forEach((esp) => {
+            deck.push(esp + tipo)
+        })
     })
-    
-}
-tipos.forEach(( tipo )=>{
-    especiales.forEach(( esp )=>{
-        deck.push( tipo + esp )
-    })
-})
 
-deck = _.shuffle( deck );
-console.log( deck )
-return deck
+    deck = _.shuffle(deck);
+    console.log(deck)
+    return deck
 
 }
 crearDeck()
 
 //Esta función me permite tomar una carta
 
-const pedirCarta = ()=>{
-    if( deck.length == 0 ){
+const pedirCarta = () => {
+    if (deck.length === 0) {
         throw 'Se te acabaron las cartas amiguito'
     }
 
     let carta = deck.pop()
-    console.log( deck )
-    
-    console.log( carta )
+    console.log(deck)
+
+    console.log(carta)
     return carta
-    }
+}
 
 
-function valorCarta( carta ){
+function valorCarta(carta) {
 
-    let valor = carta.substring( 0, carta.length -1 );
-    console.log( valor );
+    let valor = carta.substring(0, carta.length - 1);
+    console.log(valor);
     let puntos = 0;
-    if( isNaN( valor ) ){
-
-        
-        if( valor === 'A' ){
+    if (isNaN(valor)) {
+        if (valor === 'A') {
             valor = 11;
-            
-        }else{
-            valor = 10;
-        }
-        console.log( `El valor es de una letra, vale ${ valor }`);
-    
-    }else{
-        
-        console.log( "Es numero")
-        puntos = valor * 1 ;
-        console.log( puntos );
-    }
 
+        } else {
+            valor = 10;
+            return valor;
+        }
+        console.log(`El valor es de una letra, vale ${valor}`);
+
+    } else {
+
+        puntos = valor * 1;
+        // console.log( "Es numero")
+        // console.log( puntos );
+    }
+    return puntos
 
 }
- valorCarta( "ds" );
+// valorCarta( "ds" );
+
+
+//Eventos
+btnPedirCarta.addEventListener('click', () => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    player.innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `cartas/${carta}.png`
+    imgCarta.classList = 'cartas';
+
+    divJugador.append(imgCarta);
+
+    if (puntosJugador > 21) {
+        console.log(`Tienes: ${puntosJugador} puntos, perdiste`)
+        btnPedirCarta.disabled = 'true';
+        
+        
+    } else if (puntosJugador === 21) {
+        {
+            
+            console.log("Conseguiste 21 Ganaste")
+            btnPedirCarta.disabled = 'true';
+        }
+    }
+
+
+});
+
+const botonNuevo = document.createElement('button');
+divBotones.append(botonNuevo);
+botonNuevo.innerText = "Botoncito"
+botonNuevo.classList.add("btn-nuevo", "btn")
+
+
